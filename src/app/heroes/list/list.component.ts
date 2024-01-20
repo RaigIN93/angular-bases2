5988bfe779fcfcdf414c6106b3a1f1fbae1c0aa9
Ruben Inca Garcia <ea.alejandro.inca.g@gmail.com>
2024-01-20 16:07:14 -0500

    Fin de seccion 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  public heroesNames: string[] = ['Spiderman','Ironman','Hulk','She Hulk','Thor'];
+  public deletedHeroe?: string;
+  removeLastHeroe():void{
+    this.deletedHeroe = this.heroesNames.pop();
+  }
+
+}
